b86c58f2255afe67dfee5ddfe0fe8e51fa3596be
Srancsik Agi <agnes.srancsik@gmail.com>
2019-11-06 20:21:50 +0100

    fájlok feltöltése

--- test/osszead.py
+++ test/osszead.py
@@ -7,12 +7,12 @@ driver.get("https://srancsika.github.io/firstproject/")
 
 driver = webdriver.Chrome()
 driver.get("https://srancsika.github.io/firstproject/")
-driver.find_element(By.ID, "elsoAdat").send_keys("1")
-driver.find_element(By.ID, "masodikAdat").send_keys("2")
+driver.find_element(By.ID, "a-input").send_keys("1")
+driver.find_element(By.ID, "b-input").send_keys("2")
 
 header_text = driver.find_element(By.XPATH, "//h1").text
 assert header_text == "Számológép"
 
-driver.find_element(By.ID, "gomb").click()
-
-driver.close()
+driver.find_element(By.ID, "submit-button").click()
+eredmeny = driver.find_element(By.ID, "result-input").get_attribute("value")
+assert eredmeny == "3"
