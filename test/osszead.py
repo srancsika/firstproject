027d0e0b2ab8f1165d16c3aa2865627f56dcd87d
Srancsik Agi <agnes.srancsik@gmail.com>
2019-11-06 19:24:24 +0100

    fájlok feltöltése

--- test/osszead.py
+++ test/osszead.py
@@ -1,21 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome("/Users/agnessrancsik/Selenium/Masodikora_1030/chromedriver")
+driver = webdriver.Chrome("//Users//agnessrancsik//Selenium//Masodikora_1030//test//chromedriver")
 driver.get("https://srancsika.github.io/firstproject/")
 
 
-class TestOsszeadas():
-  def setup_method(self, method):
-    self.driver = webdriver.Chrome()
-    self.vars = {}
+driver = webdriver.Chrome()
+driver.get("https://srancsika.github.io/firstproject/")
+driver.find_element(By.ID, "elsoAdat").send_keys("1")
+driver.find_element(By.ID, "masodikAdat").send_keys("2")
 
-  def test_osszeadas(self):
-    self.driver.get("https://srancsika.github.io/firstproject/")
-    self.driver.set_window_size(1226, 731)
-    self.driver.find_element(By.ID, "elsoAdat").click()
-    self.driver.find_element(By.ID, "elsoAdat").send_keys("1")
-    self.driver.find_element(By.ID, "masodikAdat").click()
-    self.driver.find_element(By.ID, "masodikAdat").send_keys("2")
-    self.driver.find_element(By.ID, "gomb").click()
-    assert self.driver.find_element(By.CSS_SELECTOR, ".alert").text == "Location has saved"
+header_text = driver.find_element(By.XPATH, "//h1").text
+assert header_text == "Számológép"
+
+driver.find_element(By.ID, "gomb").click()
+
+driver.close()
