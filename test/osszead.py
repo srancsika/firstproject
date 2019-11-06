0d81a75e0416bd56373da71da0c2fd3909e4d3c9
Srancsik Agi <agnes.srancsik@gmail.com>
2019-11-06 20:27:06 +0100

    fájlok feltöltése

--- test/osszead.py
+++ test/osszead.py
@@ -1,18 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome("\\Users\\agnessrancsik\\Selenium\\Masodikora_1030\\test\\chromedriver")
-driver.get("https://srancsika.github.io/firstproject/")
-
-
-driver = webdriver.Chrome()
+driver = webdriver.Chrome("/Users/agnessrancsik/Selenium/Masodikora_1030/test/chromedriver")
 driver.get("https://srancsika.github.io/firstproject/")
 driver.find_element(By.ID, "a-input").send_keys("1")
 driver.find_element(By.ID, "b-input").send_keys("2")
 
-header_text = driver.find_element(By.XPATH, "//h1").text
-assert header_text == "Számológép"
-
 driver.find_element(By.ID, "submit-button").click()
 eredmeny = driver.find_element(By.ID, "result-input").get_attribute("value")
+
+driver.save_screenshot("result.png")
 assert eredmeny == "3"
